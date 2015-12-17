e48025b82255109f30098c5bec18b1fc60cf754a
tozkopan <akhmet.gafarov@gmail.com>
2015-12-17 11:08:10 +0300

    mvideo cafe 06

--- cafe/view/Master.controller.js
+++ cafe/view/Master.controller.js
@@ -29,6 +29,37 @@ sap.ui.controller("mvideo.cafe.view.Master", {
 	onListPress : function(evt) {
 		this.app.handleDetail(evt.getSource().getBindingContext(), "Detail", false);
 	},
+	
+	
+	onDelete : function(evt) {
+		MB = sap.m.MessageBox;
+		var listItem = evt.getParameter("listItem");
+		var oList = evt.getSource();
+		var that = this;
+		MB.confirm("Вы действительно хотите удалить льготу из корзины?",{
+			title:"Подтверждение",
+			onClose: function(action){
+				if(action == MB.Action.OK){
+					that.handleDelete(listItem, oList);
+				}
+			}
+		});
+	},
+
+	handleDelete : function(listItem, oList) {
+		//var listItem = evt.getParameter("listItem");
+		//var oList = evt.getSource();
+		var sPath = listItem.getBindingContext().getPath();
+		var oModel = oList.getModel();
+		//binding path to all items
+		var sUpPath = sPath.split("/").slice(0,-1).join("/");
+		var aItems =  oModel.getProperty( sUpPath );
+		var oItem =  oModel.getProperty( sPath );
+		aItems.splice( aItems.indexOf(oItem), 1);
+		//oList.removeItem(listItem);
+		oModel.refresh(true);
+		oList.refreshItems();
+	},
 
 	getGroupHeader : function(oGroup) {
 		var sName = mvideo.cafe.util.Formatter.getGroupHeader(oGroup.key)
